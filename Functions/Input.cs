649b1325b3e85f492a17f046f2858dc40c2dbb3e
Laurent Bel <laurent.bel@pernod-ricard.com>
2021-03-30 08:51:36 +0200

    Parsing logs to add labels

--- Functions/Input.cs
+++ Functions/Input.cs
@@ -63,11 +63,28 @@ namespace PR.Squid.KinesisToLoki
                     // Decode the base64 content
                     byte[] recordByte = Convert.FromBase64String(kinesisRecord.Data);
                     string record = Encoding.UTF8.GetString(recordByte);
-                    //log.LogInformation($"Kinesis content: {record}");
+                    
+                    // Parse the record
+                    string[] recordFragments = record.Split('\t');
+                    CloudFrontLogRecord cloudFrontLogRecord = new CloudFrontLogRecord(recordFragments);
+
 
                     // Build the Loki entry and send it
                     Dictionary<string, string> labels = new Dictionary<string, string>();
                     AddLokiLabel(labels, "project", "API Management Cloudfront");
+                    AddLokiLabel(labels, "sc-status", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("sc-status", null));
+                    AddLokiLabel(labels, "cs-method", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("cs-method", null));
+                    AddLokiLabel(labels, "cs-protocol", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("cs-protocol", null));
+                    AddLokiLabel(labels, "cs-host", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("cs-host", null));
+                    AddLokiLabel(labels, "cs-protocol-version", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("cs-protocol-version", null));
+                    AddLokiLabel(labels, "c-ip-version", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("c-ip-version", null));
+                    AddLokiLabel(labels, "x-edge-response-result-type", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("x-edge-response-result-type", null));
+                    AddLokiLabel(labels, "ssl-protocol", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("ssl-protocol", null));
+                    AddLokiLabel(labels, "ssl-cipher", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("ssl-cipher", null));
+                    AddLokiLabel(labels, "x-edge-result-type", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("x-edge-result-type", null));
+                    AddLokiLabel(labels, "x-edge-detailed-result-type", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("x-edge-detailed-result-type", null));
+                    AddLokiLabel(labels, "c-country", cloudFrontLogRecord.Content.GetValueOrDefault<string, string>("c-country", null));
+
                     LokiLogEntry lokiLogEntry = new LokiLogEntry(
                         labels,
                         DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString() + "000000",
